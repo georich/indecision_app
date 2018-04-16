738e19af3014c02f425357dbe400a7f75a8ed736
George Richards <g.richards101@gmail.com>
2018-04-16 18:50:33 +0100

    use map on options array to generate list

--- src/app.js
+++ src/app.js
@@ -37,8 +37,11 @@ const renderApp = () => {
       <p>{app.options.length}</p>
       <button onClick={clearOptions}>Remove All</button>
       <ol>
-        <li>Item one</li>
-        <li>Item two</li>
+        {
+          app.options.map((option) => {
+            return <li key={option}>{option}</li>
+          })
+        }
       </ol>
       <form onSubmit={onFormSubmit}>
         <input type="text" name="option"/>
